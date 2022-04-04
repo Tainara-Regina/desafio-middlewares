36506be40384d7ae7297c688214ffbe8e1da7d43
Tainara Regina <tainararegina20@gmail.com>
2022-04-04 20:24:27 -0300

    teste finalizado

--- src/index.js
+++ src/index.js
@@ -10,19 +10,65 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+ const user =  users.find((user) => user.username === username)
+
+ if(user){
+   request.user = user
+   return next()
+ }else{
+   return response.status(404).json({error: "usuario não encontrado"})
+ }
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { pro,todos } = request.user;
+  
+if(pro || (!pro && todos.length < 10 )){
+return next();
+}else{
+  return response.status(403).json({mensage: "usuario já atingiu o limite"})
+}
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  if(!validate(id)){
+    response.status(400).json({error: "o UUID não é valido"})
+  }
+  
+  const user = users.find((user) => user.username === username )
+  
+ if(user){
+  const todo = user.todos.find((todo) => todo.id === id )
+  if(todo){
+    request.todo = todo;
+    request.user = user;
+    return next();
+  }else{
+    response.status(404).json({error: "todo não encontrado"})
+  }
+ }else{
+   return response.status(404).json({error: "usuario não encontrado"})
+ }
+  
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+  const user = users.find(( user ) => user.id === id)
+
+  if(user){
+    request.user = user;
+    return next();
+  }else{
+    return response.status(404)
+  }
 }
 
 app.post('/users', (request, response) => {
